e03397b6ba5ca8a2ebe04dac181dd26020c98e6b
VladimirKhrapovickiy <vovandossser90@gmail.com>
2022-10-17 22:04:25 +0700

    project-1 new feature added

--- JS/project-1/color_switcher.js
+++ JS/project-1/color_switcher.js
@@ -1,5 +1,24 @@
-document.querySelector('.project_1__button').onclick = switchBackground;
-function switchBackground(){
-    let element = document.querySelector('.project_1');
-    element.classList.toggle('cool')
-  }
+let projectDiv = document.querySelector(".project_1");
+let pixelEl = document.createElement("div");
+pixelEl.className = "project_1__pixel";
+let divHeight = projectDiv.clientHeight;
+let divWidth = projectDiv.clientWidth;
+let numberOfEl = (divHeight * divWidth) / Math.pow(25, 2);
+let pixel;
+function switchBackground() {
+  let element = document.querySelector(".project_1");
+  pixel = document.querySelector(".project_1__pixel");
+  element.classList.toggle("cool");
+  pixel == null ? projectDiv.prepend(pixelEl) : pixelEl.remove();
+}
+projectDiv.prepend(pixelEl);
+pixel = document.querySelector(".project_1__pixel");
+for (let i = 0; i < numberOfEl; i++) {
+  var el = pixel.cloneNode(true);
+  projectDiv.appendChild(el);
+}
+
+document.querySelector(".project_1__button").onclick = switchBackground;
+console.log(numberOfEl);
+console.log(document.querySelector("project_1__pixel"));
+//pixel == null ? pixelEl.remove() : projectDiv.prepend(pixelEl);

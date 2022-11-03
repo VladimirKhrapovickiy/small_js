6273cf3e921ca8cd6ab0773e879724767c0fb425
VladimirKhrapovickiy <vovandossser90@gmail.com>
2022-11-03 11:05:16 +0700

    project_6 small update

--- JS/project-6/modal_window.js
+++ JS/project-6/modal_window.js
@@ -2,16 +2,16 @@ const modalWindow = document.querySelector(".modal_window");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close_modal");
 const btnCallModal = document.querySelector(".button_modal");
-const addDisplayNone = function () {
-  modalWindow.classList.toggle("display_none");
-  overlay.classList.toggle("display_none");
+const addVisibility = function () {
+  modalWindow.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
 };
-btnCallModal.addEventListener("click", addDisplayNone);
-btnCloseModal.addEventListener("click", addDisplayNone);
-overlay.addEventListener("click", addDisplayNone);
+btnCallModal.addEventListener("click", addVisibility);
+btnCloseModal.addEventListener("click", addVisibility);
+overlay.addEventListener("click", addVisibility);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
-    modalWindow.classList.add("display_none");
-    overlay.classList.add("display_none");
+    modalWindow.classList.add("hidden");
+    overlay.classList.add("hidden");
   }
 });
